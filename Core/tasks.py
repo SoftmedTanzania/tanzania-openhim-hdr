ad8b32a068156fe8fa002d4b037f7b028e96269e
Danford Nchembi <danfordnchembi@gmail.com>
2021-10-24 13:05:58 +0300

    changes

--- Core/tasks.py
+++ Core/tasks.py
@@ -14,7 +14,7 @@ from ValidationManagement import models as validation_management_models
 app = Celery()
 
 
-@app.task(time_limit=10000,soft_time_limit=8000)
+@app.task()
 def save_payload_from_csv():
     print("function was called")
     root_path = "uploads"
