0f6bcf0cca65b48ce261f5f90cdd414d5e79f05d
Danford Nchembi <danfordnchembi@gmail.com>
2021-11-18 09:44:09 +0300

    changes

--- DHIS/tasks.py
+++ DHIS/tasks.py
@@ -12,7 +12,7 @@ app = Celery()
 
 
 def send_to_dhis(payload):
-    dhis_url = config('DHIS_URL')
+    dhis_url = config('HIM_DHIS_URL')
     response = requests.post(dhis_url, auth=(config('DHIS_USERNAME'), config('DHIS_PASSWORD')),
                              json=payload)
     return response
