0377a3df380358caaed2dd5fd159e247a40d9e27
Danford Nchembi <danfordnchembi@gmail.com>
2021-11-26 16:04:31 +0300

    changes

--- ValidationManagement/admin.py
+++ ValidationManagement/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import TransactionSummary, TransactionSummaryLine, ValidationRule, FieldValidationMapping, PayloadThreshold
-
+from django import forms
+from .models import TransactionSummary, TransactionSummaryLine, ValidationRule, FieldValidationMapping, \
+    PayloadThreshold, PayloadFieldMapping
 
 # Register your models here.
-class FieldValidationMappingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'message_type','field','validation_rule')
-    search_fields = ['message_type', ]
+# class FieldValidationMappingAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'message_type','field','validation_rule')
+#     search_fields = ['message_type', ]
+#
+#     def formfield_for_foreignkey(self, db_field, request, **kwargs):
+#         if db_field.name == "message_type":
+#             kwargs["queryset"] = PayloadFieldMapping.objects.filter(message_type=self.message_type)
+#         return super(FieldValidationMappingAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 class PayloadThresholdAdmin(admin.ModelAdmin):
@@ -29,12 +35,17 @@ class ValidationRuleAdmin(admin.ModelAdmin):
     list_display = ('id','description','rule_name')
     search_fields = ['description',]
 
+
+class PayloadFieldMappingAdmin(admin.ModelAdmin):
+    list_display = ('id','message_type','field')
+    search_fields = ['message_type',]
+
     # def has_delete_permission(self, request, obj=None):
     #     return False
-
 
 admin.site.register(TransactionSummary, TransactionSummaryAdmin)
 admin.site.register(TransactionSummaryLine, TransactionSummaryLinesAdmin)
 admin.site.register(ValidationRule, ValidationRuleAdmin)
-admin.site.register(FieldValidationMapping, FieldValidationMappingAdmin)
+# admin.site.register(FieldValidationMapping, FieldValidationMappingAdmin)
 admin.site.register(PayloadThreshold, PayloadThresholdAdmin)
+# admin.site.register(PayloadFieldMapping, PayloadFieldMappingAdmin)
