ba7cf8a61a7e997dad8e9a1e1aaa1c7a5fcf2da8
Danford Nchembi <danfordnchembi@gmail.com>
2021-12-04 14:10:25 +0300

    changes

--- TerminologyServicesManagement/models.py
+++ TerminologyServicesManagement/models.py
@@ -136,24 +136,21 @@ def send_new_or_updated_icd10_code(sender, instance, created, **kwargs):
     icd10_category_description = icd10_category.description
     icd10_category_identifier = icd10_category.identifier
 
-    item = {
-        "icd10_category_identifier": icd10_category_identifier,
-        "icd10_category_description": icd10_category_description,
-        "icd10_sub_category_identifier": icd10_sub_category_identifier,
-        "icd10_sub_category_description": icd10_sub_category_description,
-        "icd10_code": icd10_diagnoses_code,
-        "icd10_code_description": icd10_description,
-        "icd10_sub_code": instance.sub_code,
-        "icd10_sub_code_description": instance.description
-    }
+    item = dict()
+
+    item["icd10_category_identifier"] = icd10_category_identifier
+    item["icd10_category_description"] = icd10_category_description
+    item["icd10_sub_category_identifier"] = icd10_sub_category_identifier
+    item["icd10_sub_category_description"] = icd10_sub_category_description
+    item["icd10_code"] = icd10_diagnoses_code
+    item["icd10_code_description"] = icd10_description
+    item["icd10_sub_code"] = instance.sub_code
+    item["icd10_sub_code_description"] = instance.description
 
     json_data = json.dumps(item)
-    print(json_data)
 
-    response = requests.post(him_icd_url,auth=(him_username, him_password),data=json_data,
+    requests.post(him_icd_url,auth=(him_username, him_password),data=json_data,
                              headers={'User-Agent': 'XY', 'Content-type': 'application/json'})
-
-    print(response.json())
 
 
 @receiver(post_save, sender=CPTCode)
@@ -169,14 +166,14 @@ def send_new_or_updated_cpt_code(sender, instance, created, **kwargs):
     cpt_code_category = CPTCodeCategory.objects.get(id=cpt_code_category_id)
     cpt_code_category_description = cpt_code_category.description
 
-    item = {
-        "cpt_category_description": cpt_code_category_description,
-        "cpt_sub_category_description": cpt_code_sub_category_description,
-        "cpt_code": cpt_code,
-        "cpt_code_description": cpt_description
-    }
+    item = dict()
+
+    item["cpt_category_description"] = cpt_code_category_description
+    item["cpt_sub_category_description"] = cpt_code_sub_category_description
+    item["cpt_code"] = cpt_code
+    item["cpt_code_description"] = cpt_description
 
     json_data = json.dumps(item)
 
-    response = requests.post(him_cpt_url, auth=(him_username, him_password),data=json_data,
+    requests.post(him_cpt_url, auth=(him_username, him_password),data=json_data,
                              headers={'User-Agent': 'XY', 'Content-type': 'application/json'})
